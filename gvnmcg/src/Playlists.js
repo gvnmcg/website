70ec1c9e56bc9d4d3650ac5182944583286656fc
Gavin <gvnmcg517@gmail.com>
2020-07-08 20:26:27 -0600

    fix iframe warning

--- gvnmcg/src/Playlists.js
+++ gvnmcg/src/Playlists.js
@@ -4,13 +4,16 @@ const Playlists = () => (
     <div>
         <label>2020</label>
         
-        <iframe src="https://open.spotify.com/embed/playlist/6rwpproOh59djscbkwdqq0" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <iframe src="https://open.spotify.com/embed/playlist/6rwpproOh59djscbkwdqq0" 
+        width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"
+        title="2020"></iframe>
         
         
         <label>Guitar inspo</label>
         
-        
-        <iframe src="https://open.spotify.com/embed/playlist/7G9Ortc7nyvGGjvawMNmmI" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <iframe src="https://open.spotify.com/embed/playlist/7G9Ortc7nyvGGjvawMNmmI" 
+        width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"
+        title="Guitar inspo"></iframe>
     </div>
 )
 
